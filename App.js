8774bcf2c6c60eb8addc04c24131fb15f15712fb
Zaakir Mungrue <mungruez@gmail.com>
2026-08-18 11:38:34 -0400

    crosswrdQuizstrtPsol

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import Resources from './screens/Resources';
-import FightersList from './screens/FightersList';
-import Fighter from './screens/Fighter';
-//import WheeProblemsAndSolutions from './screens/WheeProblemsAndSolutions';
-//import WheeChapters from './screens/WheeChapters';
+import WheeProblemsAndSolutions from './screens/WheeProblemsAndSolutions';
+import WheeChapters from './screens/WheeChapters';
 import WheeCrosswords from './screens/WheeCrosswords';
 import WheeQuizzes from './screens/WheeQuizzes';
 
@@ -18,11 +16,11 @@ export default function App() {
       <StatusBar style="dark" />
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false, animation: 'slide_from_left'}}/>
-        <Stack.Screen name="FightersList" component={FightersList} options={{headerShown:false, animation: 'slide_from_right'}}/>
-        <Stack.Screen name="FighterScreen" component={Fighter} options={{headerShown:false, animation: 'slide_from_bottom', detachPreviousScreen: false}}/>
+        <Stack.Screen name="Chapters" component={WheeChapters} options={{headerShown:false, animation: 'slide_from_right'}}/>
+        <Stack.Screen name="ProblemsAndSolutions" component={WheeProblemsAndSolutions} options={{headerShown:false, animation: 'slide_from_right'}}/>
         <Stack.Screen name="Crosswords" component={WheeCrosswords} options={{headerShown:false, animation: 'slide_from_right'}}/>
-        <Stack.Screen name="About" component={Resources} options={{headerShown:false, animation: 'slide_from_right'}}/>
         <Stack.Screen name="Quizzes" component={WheeQuizzes} options={{headerShown:false, animation: 'slide_from_right'}}/>
+        <Stack.Screen name="About" component={Resources} options={{headerShown:false, animation: 'slide_from_right'}}/>
       </Stack.Navigator>
      </NavigationContainer>
   );
